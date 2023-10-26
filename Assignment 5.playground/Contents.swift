7f5d9dca66b23b7d4c2509801d841822121d8daa
russellcampbell88 <148352935+russellcampbell88@users.noreply.github.com>
2023-10-26 10:46:07 -0400

    Update Contents.swift

--- Assignment 5.playground/Contents.swift
+++ Assignment 5.playground/Contents.swift
@@ -5,7 +5,7 @@ var greeting = "Hello, playground"
 //  Assignment-5
 /* Create new playground taking any Example of your choice, so as to display the use of
 Below in Swift
-1..Generics for classes, enum, struct
+1.Generics for classes, enum, struct
 2.Higher order functions
 3.Closures AND its types
 Prepare Theoretical part for above topics as well, We will have question answer session for it.
@@ -14,9 +14,10 @@ Prepare Theoretical part for above topics as well, We will have question answer 
 //**Problem 1. - Generics for Classes and Structs
 
 //  Generics for Classes
-//  by adding T, we make the class work with any datatpe
-class Information< T > {
-    
+//  by adding T, we make the class usable with any datatpe.
+class GenericInformation< T > {
+    //  When specifying the datatype to T, we generalize the var to handle any
+    //  kind of data.
     var data: T
     
     init( data: T ) {
@@ -34,7 +35,7 @@ class Information< T > {
 //  Generic form.
 
 //  Remember, Queue is FIFO
-struct MyQueue< T > {
+struct GenericQueue< T > {
     
     var myqueue: [T]
     
@@ -68,15 +69,17 @@ let evenNumber = numbers.filter{ $0 % 2 != 0 }
 print("evenNumber- \(evenNumber)")
 
 //  MARK: Reduce
+//  Reduce temporarily stores a value.
 let totalOfAllNumbers = numbers.reduce(0, +)
 print("totalOfAllNumbers is- \(totalOfAllNumbers)")
 
 //  MARK: Sort
-let unsortedNumbers = [234, 44, 11, 76, 97, 3, 1, 123553, 45]
+let unsortedNumbers = [987, 33, 5, 76, 97, 3, 1, 199873, 4522]
 let sortedValues = unsortedNumbers.sorted()
 print( sortedValues )
 
 //  MARK: FlatMap
+//  Separates each element into a separate array. Flattens the array
 let someWords = [ "Hello", "World","Swift" ]
 let resul = someWords.flatMap{$0}
 
@@ -87,15 +90,17 @@ print( duplicateNilArray )
 
 //**Problem 3. Closures --
 /*
- *
- * A closure is an unnamed block of code that we can pass around and use in code and can assign to a var or can return from a function size
+ * A closure is an unnamed block of code that we can pass around and use in code and can assign to a var or can return from a function size. You can pass closures as function arguments. They are essentially unnamed functions.
+ * Closures are self-contained blocks of functionality that can be passed around in other
+ * parts of the code.
  *
  */
 
 //  Closures are reference types.
 //  They capture and store the reference to any constants in the surrounding context.
+//  Basically, they're functions that have environments of their own.
 
-// i. Non-Escaping closures
+// i. Non-Escaping Closures
 func squareThisNumber( number1: Int ) -> Int {
     return number1 * number1
 }
@@ -114,30 +119,28 @@ func doSquare( number1: Int, completion:(Int)->()){
 }
 doSquare( number1: 5, completion: { output in
     print("doSquare- \(output)")
-}
-
-
+})
         
-// ii. Escaping Closures
-func performAPICall( completion: () -> () ) {
-    print( "flag that we're in performAPICall")
-    
-    DispatchQueue.global().async {
-        print("2. We are inside global Queue" )
+    // ii. Escaping Closures
+    func performAPICall( completion: () -> () ) {
+        print( "flag that we're in performAPICall")
         
-        Thread.sleep( forTimeInterval: 4 )
-        
-        DispatchQueue.main.async {
-            print( "3. We are inside DispatchQueue.main" )
-            print( "4. After a 4-second delay" )
+        DispatchQueue.global().async {
+            print("2. We are inside global Queue" )
+            
+            Thread.sleep( forTimeInterval: 4 )
+            
+            DispatchQueue.main.async {
+                print( "3. We are inside DispatchQueue.main" )
+                print( "4. After a 4-second delay" )
+            }
+            print( "5. After DispatchQueue main" )
         }
-        print( "5. After DispatchQueue main" )
     }
-}
-    performAPICall {
-        print( "Once we get a response from performAPICall." )
-    }
-    
+        performAPICall {
+            print( "Once we get a response from performAPICall." )
+        }
+          
     // iii. Trailing Closure
     /*
      *   Trailing Closures occur whenever there is a closure which is
@@ -157,7 +160,7 @@ func performAPICall( completion: () -> () ) {
     /* When a closure doesn't take any parameter;
      * it enables you to define an argument that
      * automatically gets wrapped in a closure.
-     * It'ss used inside Swift wherever code needs to be passed in
+     * It's used inside Swift wherever code needs to be passed in
      * and executed only if conditions are right     */
     
     //  It returns anything from a closure, then it gets self-wrapped when when called, this ins known as an Auto Closure. It doesn't accept, doesn't return

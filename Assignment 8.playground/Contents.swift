93e24a8166fd1b096cc90ee54ab38fe83c712b7e
russellcampbell88 <148352935+russellcampbell88@users.noreply.github.com>
2023-10-31 07:53:12 -0400

    Update Assignment 8

--- Assignment 8.playground/Contents.swift
+++ Assignment 8.playground/Contents.swift
@@ -3,37 +3,56 @@ import UIKit
 var greeting = "Hello, playground"
 
 //  MARK: CONCURRENT QUEUES
+
+//**Problem 1.
+
+//  A concurrent queue allows us to execute multiple tasks at the same time.
+
+//  Tasks will always start in the order they’re added but they can finish in a different order as they can be executed in parallel. 
+
+//  Tasks will run on distinct threads that are managed by the dispatch queue. The number of tasks running at the same time is variable and depends on system conditions.
+
 //  Concurrent Queues are the only ones where you can mix sync/async
 let concurrentQueue = DispatchQueue( label:"ConcurrentQueue")
 
 concurrentQueue.sync {
-    for _ in 0...1000{
+    for _ in 0...1000 {
         
     }
     print( "a. Task")
 }
 concurrentQueue.sync {
-    for _ in 0...50{
+    for _ in 0...50 {
 
     };
     print("b. Task")
 }
 concurrentQueue.async {
-    for _ in 0...1{
+    for _ in 0...1 {
         
     };
     print("c. Task")
 }
 
+//  MARK: GLOBAL QUEUES
+
 //**Problem 2. Global Queues
+
+//  Global concurrent queues schedule blocks as soon as
+//  threads become available ("non-FIFO" completion order).
 
 //  Instead of main, use global. Global queues
 //  are queues provides by the OS.
 //  They're always concurrent queues.
+
+    //  1. Tasks in concurrent queue executes concurrently [background threads]
+    //  2. Tasks are still started in the order that they were added to the queue
 DispatchQueue.global().async {
-    print("Global Queue")
+    print("This is our Global Queue")
 }
 
+//  global(qos: )
+//  Returns the global system queue with the specified quality-of-service class.
 
 //  i. User Interactive
 DispatchQueue.global(qos: .userInteractive).async {
@@ -41,7 +60,7 @@ DispatchQueue.global(qos: .userInteractive).async {
 }
 
 //  ii. User Initiated
-DispatchQueue.global(qos: .userInitiated ).async{
+DispatchQueue.global(qos: .userInitiated ).async {
     print(" This action is user initiated.") // any work user has started.
     //  The User requires immediate results - such as pull to refresh.
 }
@@ -52,48 +71,62 @@ DispatchQueue.global(qos: .utility).async {
 }
 
 //  iv. Background
-DispatchQueue.global( qos: .background ).async{
+DispatchQueue.global( qos: .background ).async {
     print("background")
     // only specific when user is not aware
     // For tasks the user shouldn't be aware of at all.
 }
 
 //  v. Unspecified
-DispatchQueue.global( qos: .unspecified ).async{
-    print("Unspecified")
+DispatchQueue.global( qos: .unspecified ).async {
+    print( "Unspecified" )
     
 }
 
-//  MARK:
+//  MARK: OPERATION QUEUE
+
+//**Problem 3. Operation Queues
+
 //  OperationQueue - queues that were built on top of the GCD
 //  it gives more control to the user.
 //  You can pause, resume, stop, and start tasks.
 //  Add or define dependencies within your task.
 
-let taskLettuce = BlockOperation {
-    print( "Adding lettuce to salad." )
+let addCayenne = BlockOperation 
+{
+    print( "Adding cayenne to curry." )
 }
 
-let taskTomato = BlockOperation {
-    print( "Adding Tomato to Salad." )
+let addTumeric = BlockOperation 
+{
+    print( "Adding tumeric to curry." )
 }
 
-let taskOnion = BlockOperation {
-    print( "Adding Onion to Salad" )
+let addCinnamon = BlockOperation 
+{
+    print( "Adding cinnamon to curry." )
 }
 
 let operationQueue = OperationQueue()
 
-taskTomato.addDependency( taskOnion )
-taskLettuce.addDependency( taskTomato )
-operationQueue.addOperations([taskLettuce, taskTomato, taskOnion], waitUntilFinished: false)
+addTumeric.addDependency( addCinnamon )
+addCayenne.addDependency( addTumeric )
+operationQueue.addOperations( [addCayenne, addTumeric, addCinnamon ], waitUntilFinished: false )
     
 
 operationQueue.maxConcurrentOperationCount = 1
 
-// MARK: Await Async -- added in iOS 13 in 2019
+
+//  MARK: Await Async -- Added in iOS 13 in 2019
+
+//**Problem 3. Await Async
+
+//  Async means asynchronous and can be seen as a method attribute making it clear that a method performs asynchronous work.
+//
+//  Await is the keyword to be used for calling async methods. The await keyword will always go with
+//  asynchronous calls
 func task1() async {
-    print("Task 1")
+    print( "Task 1" )
     for _ in 0...1000345 {
         
     }
@@ -106,19 +139,24 @@ func task2() async {
     }
 }
 
+//  Await gives us Structured Concurrency; with async-await method calls makes it easier to reason the order of execution. Methods are linearly executed without going back and forth, like with closures.
 Task {
     await task1( )
     await task2( )
 }
 
-//  MARK: Actors
+//  MARK: ACTORS
+
+//**Problem 4. Actors
 
 /*
- * Actors [include] properties, functions, initializers
+ *  Actors -- properties, functions, initializers
  *  (constructors), deinitializers.
  *
  *  Actors do not support iheritance.
  *  Actors guarantee properties will be modified one at a time.
+ 
+ *   One of the core advantages of Swift’s actor types is that they can help prevent “data races” — that is, memory corruption issues that can occur when two separate threads attempt to access or mutate the same data at the same time.
  *
  */
 actor BankDetails {
@@ -139,12 +177,38 @@ actor BankDetails {
     }
 }
 
+let bankDetails = BankDetails( balance: 5000 )
 
-let bankDetails = BankDetails( balance: 500 )
 
+//  Actors require the use of await
 Task {
     await bankDetails.deposit( amount: 100 )
+}
+
+Task {
     await bankDetails.deposit( amount: 50  )
+}
+
+Task {
     await bankDetails.deposit( amount: 200 )
+}
+
+/*Actors work much like classes (that is, they are passed by reference), with two key exceptions:
+* An actor automatically serializes all access to its properties and methods, which ensures that only one caller can directly interact with the actor at any given time. That in turn gives us complete protection against data races, since all mutations will be performed serially, one after the other.
+
+* Actors don’t support subclassing since, well, they’re not actually classes.
+*
+* So, practically, all that we have to do to convert our UserStorage class into an actor is to go back to its original implementation, and simply replace its use of the class keyword with the new actor keyword:
+ */
+actor UserStorage {
+    private var users: [String] = []
+
+    func store() {
+        for user in users {
+            print( "All users:", user ) // string interpolation can only appear in literal
+        }
+    }
+
+
 }
 //  actor CurrentBankDetails: BankDetails{}

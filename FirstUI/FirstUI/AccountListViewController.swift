df44070febb65ad077922d580c632b0a9899cfea
russellcampbell88 <148352935+russellcampbell88@users.noreply.github.com>
2023-11-01 06:01:47 -0400

    Update FirstUI commit

---
+++ FirstUI/FirstUI/AccountListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountListViewController.swift
+//  FirstUI
+//
+//  Created by Russell Campbell on 10/31/23.
+//
+
+import UIKit
+
+class AccountListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
